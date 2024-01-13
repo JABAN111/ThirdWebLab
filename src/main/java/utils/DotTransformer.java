c8bba0f812203abfdc695f59009df1949d865981
JABAN111 <368601@edu.itmo.ru>
2024-01-14 01:02:09 +0300

    Добавлена обработка Double и Float через джинерики

--- src/main/java/utils/DotTransformer.java
+++ src/main/java/utils/DotTransformer.java
@@ -1,8 +1,15 @@
 package utils;
 
 public class DotTransformer {
-    public static Double dotTransform(Double num){
-        String strNum = num.toString().replace(",", ".");
-        return Double.parseDouble(strNum);
+
+    public static <T extends Number> T dotTransform(T num){
+        String strNum = num.toString().replace(",",".");
+        if (num instanceof Double) {
+            return (T) Double.valueOf(strNum);
+        } else if (num instanceof Float) {
+            return (T) Float.valueOf(strNum);
+        } else {
+            throw new IllegalArgumentException("Unsupported Number type");
+        }
     }
 }
