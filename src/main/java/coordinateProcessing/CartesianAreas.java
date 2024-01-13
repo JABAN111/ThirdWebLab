ad30327b05cd03e7dc6b41af77dc164b2eebe578
JABAN111 <368601@edu.itmo.ru>
2024-01-14 01:32:47 +0300

    Убраны модификаторы доступа за ненадобностью

--- src/main/java/coordinateProcessing/CartesianAreas.java
+++ src/main/java/coordinateProcessing/CartesianAreas.java
@@ -1,9 +1,9 @@
 package coordinateProcessing;
 
 public interface CartesianAreas {
-    public  boolean firstRotation(Integer x,Double y,Float R);
-    public boolean secondRotation(Integer x,Double y,Float R);
-    public boolean thirdRotation(Integer x,Double y,Float R);
-    public boolean fourthRotation(Integer x,Double y,Float R);
-    public boolean areaCheck(Integer x,Double y,Float R);
+    boolean firstRotation(Integer x, Double y, Float R);
+    boolean secondRotation(Integer x, Double y, Float R);
+    boolean thirdRotation(Integer x, Double y, Float R);
+    boolean fourthRotation(Integer x,Double y,Float R);
+    boolean areaCheck(Integer x,Double y,Float R);
 }
