94736f96bd579af4b085e98238522a8583c5322d
JABAN111 <368601@edu.itmo.ru>
2024-01-23 19:30:38 +0300

    Изменен типы данных Добавлены getter Setter через lombok 'почищен' код

--- src/main/java/resultProcessingBean/ResultController.java
+++ src/main/java/resultProcessingBean/ResultController.java
@@ -3,8 +3,9 @@ package resultProcessingBean;
 import coordinateProcessing.AreaProcessing;
 import dataBaseHandler.ResultEntity;
 import dataBaseHandler.service.resultServiceImpl;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.Data;
+import org.primefaces.PrimeFaces;
+import utils.DotTransformer;
 
 import javax.faces.bean.ApplicationScoped;
 import javax.faces.bean.ManagedBean;
@@ -13,28 +14,18 @@ import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
+import java.util.Locale;
 
 
 @ManagedBean(name = "Controller", eager = true)
 @ApplicationScoped
+@Data
 public class ResultController implements Serializable {
-    @Getter
-    @Setter
     private volatile List<ResultEntity> results;
     private final AreaProcessing areaProcessing = new AreaProcessing();
     private final resultServiceImpl resultService = new resultServiceImpl();
-    @Getter
-    @Setter
-    private Integer xtest;
-    @Getter
-    @Setter
-    private Double ytest;
-    @Getter
-    @Setter
-    private Float rtest;
-    @Getter
-    @Setter
     private boolean hitStatus;
+
     public ResultController(){
         if(results == null)
             results = new ArrayList<>();
@@ -46,35 +37,26 @@ public class ResultController implements Serializable {
             results = new ArrayList<>();
         results.add(entity);
     }
-    private void saveRes(Integer x, Double y, Float R){
-        this.xtest = x;
-        this.ytest=y;
-        this.rtest=R;
-    }
+
     private void getAllResults(){
         results = resultService.getAllResults();
     }
-    public void addResult(Integer x, String y_value, Float R){
-        if(y_value == null){
-            System.err.println("y_value is null");
+
+    public void addResult(Double x, String y_value, Float R){
+        if(y_value == null || x == null || R==null)
             return;
-        }
-        Double y = Double.parseDouble(y_value);
-        System.out.println("Got data");
-        System.out.println("x = " + x + " y = " + y + " R = " +R);
-        if(x != null && R != null){
-            saveRes(x,y,R);
-            Date date = new Date();
-            hitStatus = areaProcessing.areaCheck(x,y,R);
-            ResultEntity result = new ResultEntity(x,y,R,date, hitStatus);
-            saveResultLocal(result);
-            resultService.addResult(result);
-        }
-        else
-            System.err.println("Some info is null");
 
+        x = Math.floor(x * 1000) / 1000;
+        Double y = DotTransformer.dotTransform(y_value);
+
+        Date date = new Date();
+        hitStatus = areaProcessing.areaCheck(x,y,R);
+        ResultEntity result = new ResultEntity(x,y,R,date, hitStatus);
+        saveResultLocal(result);
+        resultService.addResult(result);
+        String script = String.format(Locale.US,"window.printDotOnGraph(%f,%f,%b);",x,y,hitStatus);
+        PrimeFaces.current().executeScript(script);
     }
-
     public void cleanResults(){
         if(results != null)
             results.clear();
