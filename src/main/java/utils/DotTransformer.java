6e3cc3ed2f9f8f4974e4a77d251e1b1c34130625
JABAN111 <368601@edu.itmo.ru>
2024-01-23 18:44:25 +0300

    dotTranform теперь возвращает Double в соответствии с параметром y

--- src/main/java/utils/DotTransformer.java
+++ src/main/java/utils/DotTransformer.java
@@ -2,8 +2,8 @@ package utils;
 
 public class DotTransformer {
 
-    public static Float dotTransform(String num){
+    public static Double dotTransform(String num){
         String strNum = num.replace(",",".");
-        return Float.parseFloat(strNum);
+        return Double.parseDouble(strNum);
     }
 }
