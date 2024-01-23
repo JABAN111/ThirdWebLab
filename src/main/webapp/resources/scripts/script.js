662f97c722a496f03018552b67187f20ad2b49b7
JABAN111 <368601@edu.itmo.ru>
2024-01-23 19:47:47 +0300

    почищен код

--- src/main/webapp/resources/scripts/script.js
+++ src/main/webapp/resources/scripts/script.js
@@ -1,7 +1,8 @@
 document.addEventListener("DOMContentLoaded",function () {
-    //default radius after loading page
     setRadius(2);
     canvas.addEventListener("click", mouseHandler);
+    deleteButton.addEventListener('click',deleteDots);
+    drawAllPointsFromResultTable();
     function mouseHandler(event) {
         if (currentRadius === null) {
             return;
@@ -14,41 +15,25 @@ document.addEventListener("DOMContentLoaded",function () {
     function clickingProcessing(clicked_x, clicked_y) {
         let x = parseFloat((((clicked_x - w / 2) / hatchGap) / 2) * currentRadius).toFixed(3);
         let y = parseFloat((-((clicked_y - h / 2) / hatchGap) / 2) * currentRadius).toFixed(3);
-        sendCoordinates(x, y)
+        sendCoordinates(x, y);
         return {
             x: x,
             y: y,
         }
     }
 });
-
 let xVal = document.getElementById("clicking-form:xHidden");
 let yVal = document.getElementById("clicking-form:clickingYHidden");
 let sbButton = document.getElementById('clicking-form:click-submit');
-let y_ACTUAL = document.getElementById('main-form:yValue');
-
-/**
- * Clicking hidden button with some processing :
- * 1) direct click
- * 2) Draw new point(for now old points)
- */
-function submitHiddenClick(){
-    sbButton.click();
-    drawAllPointsFromResultTable();
+let deleteButton = document.getElementById('deleteButton');
+function deleteDots(){
+    redrawGraph();
 }
-
 function sendCoordinates(xValue,yValue){
     xVal.value = xValue;
-    y_ACTUAL.value = yValue;
     yVal.value = yValue;
-    submitHiddenClick();
+    sbButton.click();
 }
-
-
-/**
- * Drawing old points from the table
- * Warning: doesn't include the last one
- */
 function drawAllPointsFromResultTable(){
     let i = 0;
     while (true){
@@ -58,9 +43,7 @@ function drawAllPointsFromResultTable(){
         let y = document.getElementById('results-table:'+ i + ':yResult');
         let result = document.getElementById('results-table:' + i + ':hitResult');
         let isHit = result.innerHTML === '✅';
-        console.log("current input: ",x.innerHTML,y.innerHTML,result.innerHTML,isHit);
         printDotOnGraph(x.innerHTML,y.innerHTML,isHit);
         i++;
     }
-    console.log('Всего отрисовано: ' + i);
 }
